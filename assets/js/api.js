68ad696cabb88380aa644c23c96dc10304406aab
rcshinka <rafael.cazuo777@gmail.com>
2025-08-29 11:14:49 -0300

    Changes in JS api and main

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,8 @@
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/rcshinka/js-portfolio/refs/heads/main/data/profile.json?token=GHSAT0AAAAAADIZY62POLM7YK6EXQH2RNVA2FMXADA';
+    const url = 'https://raw.githubusercontent.com/rcshinka/js-portfolio/refs/heads/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
+
+// raw da DIO https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json 
+// https://raw.githubusercontent.com/rcshinka/js-portfolio/refs/heads/main/data/profile.json?token=GHSAT0AAAAAADIZY62POLM7YK6EXQH2RNVA2FMXADA
